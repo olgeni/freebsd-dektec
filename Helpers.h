14b9cac52a439daee7741ce792f1479135577458
olgeni <olgeni@olgeni.com>
2012-07-26 11:31:41 +0200

    Remove unused READ_WORD and WRITE_WORD macros.

--- Helpers.h
+++ Helpers.h
@@ -22,11 +22,9 @@
 // PCI write/read helpers
 
 #define WRITE_BYTE(val, tag, handle, offset) bus_space_write_1 (tag, handle, offset, val)
-#define WRITE_WORD(val, tag, handle, offset) bus_space_write_2 (tag, handle, offset, val)
 #define WRITE_LONG(val, tag, handle, offset) bus_space_write_4 (tag, handle, offset, val)
 
 #define READ_BYTE(tag, handle, offset) bus_space_read_1 (tag, handle, offset)
-#define READ_WORD(tag, handle, offset) bus_space_read_2 (tag, handle, offset)
 #define READ_LONG(tag, handle, offset) bus_space_read_4 (tag, handle, offset)
 
 //-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.- Data declaration -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
